d2726a84579c8f738a1550b46bc0fe92875e64d1
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-08-28 14:54:22 +0200

    yo

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
+import {React, useEffect} from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from "./pages/HomePage"
-
+import Home from "./pages/HomePage";
+import { HelmetProvider } from 'react-helmet-async';
 
 const App = () => {
   return (
-        <Router>
-          <div className="flex flex-col xl:gap-y-12 w-full">
-            <Routes>
-              <Route path="/" element={<Home />} />
-            </Routes>
-          </div>
-        </Router>
+    <HelmetProvider>
+      <Router>
+        <div className="flex flex-col xl:gap-y-12 w-full">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {/* Ajoutez d'autres routes ici pour vos autres pages */}
+          </Routes>
+        </div>
+      </Router>
+    </HelmetProvider>
   );
 }
 
