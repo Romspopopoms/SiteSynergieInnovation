3028bed6c25e9fc5606ff920d9ffa0080df34c46
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-08-30 17:37:58 +0200

    yo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
     return (
-    <div className="min-h-24 w-full grid grid-cols-2 bg-[#FFFFFF]">
+    <div className="min-h-24 w-full flex flex-col space-y-4 md:space-y-0 md:my-0 justify-center items-center md:grid md:grid-cols-2 bg-[#FFFFFF]">
         <div className="flex justify-center md:justify-start items-center ml-8">
         <h1 className="font-inter text-black text-center">© Synergie Innovation 2024</h1>
         </div>
