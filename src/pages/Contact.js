265828656bc8420908e319d710625c47a382a141
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-09-17 10:33:32 +0200

    Initial commit

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -12,7 +12,7 @@ const Contact = () => {
     });
 
     const [menuOpen, setMenuOpen] = useState(false); // État pour gérer l'ouverture du menu
-    const [selectedSubject, setSelectedSubject] = useState("Sélectionnez"); // État pour gérer le sujet sélectionné
+    const [selectedSubject, setSelectedSubject] = useState(""); // État pour gérer le sujet sélectionné
 
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -72,95 +72,101 @@ const Contact = () => {
             className="w-full min-h-screen bg-center bg-cover bg-no-repeat"
             style={{ backgroundImage: `url(${BG})` }} // Vérifiez si l'ajout de `.default` résout le problème
         >
-            <div className="min-h-screen flex items-center justify-center py-12">
+            <div className="min-h-screen flex items-center justify-center">
                 <form 
                     onSubmit={handleSubmit} 
                     className="p-8 w-full max-w-xl space-y-8"
                 >
                     <div className="flex flex-col justify-center items-center">
-                    <h1 className="text-7xl font-bold font-afacad text-center text-white w-full">Contactez-nous !</h1>
-                    <p className="text-center text-white font-afacad text-3xl">Et obtenez votre devis</p>
+                        <h1 className="text-7xl font-bold font-afacad text-center text-white w-full">Contactez-nous !</h1>
+                        <p className="text-center text-white font-afacad text-3xl">Et obtenez votre devis</p>
                     </div>
                     <div className="flex space-x-4 justify-center items-center">
                         <div className="flex flex-col w-full">
                             <h2 className="text-white font-afacad mb-1 font-medium text-xl">Nom</h2>
-                        <input
-                            type="text"
-                            name="firstName"
-                            placeholder="Nom"
-                            value={formData.firstName}
-                            onChange={handleChange}
-                            required
-                            className="w-full px-4 py-2 border rounded-md bg-white text-black placeholder-black border-white font-afacad"
-                        />
+                            <input
+                                type="text"
+                                name="firstName"
+                                placeholder="Doe"
+                                value={formData.firstName}
+                                onChange={handleChange}
+                                required
+                                className="w-full px-4 py-2 border rounded-md bg-white text-black placeholder-gray-400 border-white font-afacad"
+                            />
                         </div>
                         <div className="flex flex-col w-full">
-                            <h2 className="text-white font-afacad mb-1 font-medium text-xl">Prénom</h2>
-                        <input
-                            type="text"
-                            name="lastName"
-                            placeholder="Prénom"
-                            value={formData.lastName}
-                            onChange={handleChange}
-                            required
-                            className="w-full px-4 py-2 border rounded-md bg-white text-black placeholder-black border-white font-afacad"
-                        />
-                    </div>
-                    </div>
-
-                    <div className="relative w-full">
-                        {/* Icône à droite qui tourne en fonction de l'état */}
-                        <FaChevronDown 
-                            className={`absolute right-3 top-1/2 transform -translate-y-1/2 pointer-events-none text-white transition-transform duration-300 ${menuOpen ? 'rotate-180' : ''}`} 
-                        />
-
-                        {/* Affichage de l'option sélectionnée */}
-                        <div 
-                            className="w-full px-4 py-2 border rounded-md bg-white text-black cursor-pointer relative flex justify-between items-center"
-                            onClick={toggleMenu} // Ouvre ou ferme le menu
-                        >
-                            {selectedSubject}
+                            <h3 className="text-white font-afacad mb-1 font-medium text-xl">Prénom</h3>
+                            <input
+                                type="text"
+                                name="lastName"
+                                placeholder="John"
+                                value={formData.lastName}
+                                onChange={handleChange}
+                                required
+                                className="w-full px-4 py-2 border rounded-md bg-white text-black placeholder-gray-400 border-white font-afacad"
+                            />
                         </div>
+                    </div>
+                    
+                    <div className="flex flex-col w-full">
+                        <h4 className="text-white font-afacad mb-1 font-medium text-xl">Votre demande concerne :</h4>
+                        <div className="relative w-full">
+                            {/* Icône à droite qui tourne en fonction de l'état */}
+                            <FaChevronDown 
+                                className={`absolute right-3 top-1/2 transform -translate-y-1/2 pointer-events-none text-white transition-transform duration-300 ${menuOpen ? 'rotate-180' : ''}`} 
+                            />
 
-                        {/* Liste déroulante */}
-                        {menuOpen && (
-                            <div className="absolute w-full mt-2 bg-transparent shadow-md rounded-lg backdrop-blur-[160px] ">
-                                <ul className="space-y-2">
-                                    {["Site web", "Logo", "Charte graphique", "Communication", "Photo", "Autre"].map((item, index) => (
-                                        <li 
-                                            key={index} 
-                                            className="px-4 py-2 cursor-pointer hover:bg-gray-200 text-white text-center font-afacad text-xl"
-                                            onClick={() => handleSelect(item)} // Sélectionne l'élément
-                                        >
-                                            {item}
-                                        <hr className="border-b h-px bg-gradient-to-r from-transparent via-white to-transparent mt-2" />
-                                        </li>
-                                    ))}
-                                </ul>
+                            {/* Affichage de l'option sélectionnée avec la gestion correcte du placeholder */}
+                            <div 
+                                className={`w-full px-4 py-2 border rounded-md cursor-pointer flex justify-between items-center 
+                                ${selectedSubject === "" ? 'text-gray-400' : 'text-black'} bg-white`}
+                                onClick={toggleMenu} // Ouvre ou ferme le menu
+                            >
+                                {selectedSubject === "" ? "Sélectionnez" : selectedSubject}
                             </div>
-                        )}
+
+                            {/* Liste déroulante */}
+                            {menuOpen && (
+                                <div className="absolute w-full mt-2 bg-transparent shadow-md rounded-lg backdrop-blur-[160px] ">
+                                    <ul className="space-y-2">
+                                        {["Site web", "Logo", "Charte graphique", "Communication", "Photo", "Autre"].map((item, index) => (
+                                            <li 
+                                                key={index} 
+                                                className="px-4 py-2 cursor-pointer hover:bg-gray-200 text-white text-center font-afacad text-xl"
+                                                onClick={() => handleSelect(item)} // Sélectionne l'élément
+                                            >
+                                                {item}
+                                                <hr className="border-b h-px bg-gradient-to-r from-transparent via-white to-transparent mt-2" />
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </div>
+                            )}
+                        </div>
                     </div>
 
-                    <div>
+                    <div className="flex flex-col w-full">
+                        <h5 className="text-white font-afacad mb-1 font-medium text-xl">Mail</h5>
                         <input
                             type="email"
                             name="email"
-                            placeholder="Mail"
+                            placeholder="JohnDoe@gmail.com"
                             value={formData.email}
                             onChange={handleChange}
                             required
-                            className="w-full px-4 py-2 border rounded-md bg-white text-black placeholder-black border-white font-afacad"
+                            className="w-full px-4 py-2 border rounded-md bg-white text-black placeholder-gray-400 border-white font-afacad"
                         />
                     </div>
 
-                    <div>
+                    <div className="flex flex-col w-full">
+                        <h6 className="text-white font-afacad mb-1 font-medium text-xl">Votre message</h6>
                         <textarea
                             name="message"
-                            placeholder="Message"
+                            placeholder="Bonjour, nous souhaitons créer un site vitrine 5 pages sur mesure, pour notre entreprise, pouvons-nous avoir d'avantages d'informations sur vos pratiques?"
                             value={formData.message}
                             onChange={handleChange}
                             required
-                            className="w-full px-4 py-2 border rounded-md h-32 bg-white text-black placeholder-black border-white font-afacad"
+                            className="w-full px-4 py-2 border rounded-md h-32 bg-white text-black placeholder-gray-400 border-white font-afacad"
                         ></textarea>
                     </div>
 
