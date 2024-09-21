2644480f987e1d5491d448e5d19dc8ed48844bb5
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-09-21 23:32:45 +0200

    Initial commit

--- src/components/Synergie/Section4.js
+++ src/components/Synergie/Section4.js
@@ -3,7 +3,6 @@ import { useInView } from "framer-motion";
 import AnimatedTitle from "../../components/Animated";
 import Imma from "../../assets/Logo ImaMissio W.svg";
 import Vox from "../../assets/Logo vox W.svg";
-import Scan from "../../assets/LogoScan.webp";
 
 const Section4 = () => {
     const ref = useRef(null);
@@ -26,18 +25,12 @@ const Section4 = () => {
                     Nos services web pour vos besoins
                 </h1>
             )}
-            <div className="grid grid-cols-2 sm:grid-cols-3 gap-4 md:gap-12 mt-12 md:mt-24 max-w-[80%]">
+            <div className="flex gap-4 md:gap-12 mt-12 md:mt-24 max-w-[80%]">
                 <a href="/VoxUnity" className="flex justify-center">
-                    <img src={Vox} alt="Vox" className="h-auto w-24 md:w-40"></img>
+                    <img src={Vox} alt="Vox" className="h-auto w-24 md:w-32"></img>
                 </a>
-                <a href="/ImmaMissio" className="hidden md:flex justify-center">
-                    <img src={Imma} alt="Imma" className="h-auto w-20 md:w-32"></img>
-                </a>
-                <a href="/ScanAvis" className="flex justify-center">
-                    <img src={Scan} alt="Scan" className="h-auto w-24 md:w-40"></img>
-                </a>
-                <a href="/ImmaMissio" className="md:hidden flex justify-center col-span-2">
-                    <img src={Imma} alt="Imma" className="h-auto w-36 mt-4"></img>
+                <a href="/ImmaMissio" className="flex justify-center">
+                    <img src={Imma} alt="Imma" className="h-auto w-20 md:w-72"></img>
                 </a>
             </div>
         </div>
